05280f2d968d984759e554746e1b7744a8528ef9
Manuel werder <manuel@docomondo.com>
2021-12-03 12:06:35 +0100

    Add eslint

--- day1/index.js
+++ day1/index.js
@@ -1,40 +1,39 @@
-const fs = require('fs')
+const fs = require('fs');
 
 const input = fs.readFileSync('./input.txt', 'utf-8')
-.trim()
-.split('\n');
+	.trim()
+	.split('\n');
 
 // DAY 1
 
 function getIncreases(arr) {
 
-  let count = 0;
-  for (let i = 1; i<numbers.length; i++) {
+	let count = 0;
+	for (let i = 1; i<arr.length; i++) {
 
-    if (numbers[i] > numbers[i-1]) {
-      count++;
-    }
-  }
-  return count;
+		if (arr[i] > arr[i-1]) {
+			count++;
+		}
+	}
+	return count;
 }
-
 
 function getIncreases2(arr) {
 
-  let increases = 0;
+	let increases = 0;
 
-  for (let i = 3; i<arr.length; i++) {
-    const prev = arr[i-3] + arr[i-2] + arr[i-1]
-    const current = arr[i-2] + arr[i-1] + arr[i]
-    if (current > prev) {
-      increases++;
-    }
-  }
+	for (let i = 3; i<arr.length; i++) {
+		const prev = arr[i-3] + arr[i-2] + arr[i-1];
+		const current = arr[i-2] + arr[i-1] + arr[i];
+		if (current > prev) {
+			increases++;
+		}
+	}
 
-  return increases;
+	return increases;
 }
 
-const numbers = input.map(el => parseInt(el))
+const numbers = input.map(el => parseInt(el));
 
-console.log('result1:', getIncreases(numbers))
-console.log('result2:', getIncreases2(numbers))
+console.log('result1:', getIncreases(numbers));
+console.log('result2:', getIncreases2(numbers));
--- day3/index.js
+++ day3/index.js
@@ -1,58 +1,58 @@
-const fs = require('fs')
+const fs = require('fs');
 
 const input = fs.readFileSync('./input.txt', 'utf-8')
-.trim()
-.split('\n');
+	.trim()
+	.split('\n');
 
 // DAY 3
 
 function getPowerConsumption() {
 
-  let gamma = '';
-  let epsilon = '';
-  for (let n = 0; n<input[0].length; n++) {
+	let gamma = '';
+	let epsilon = '';
+	for (let n = 0; n<input[0].length; n++) {
 
-    let ones = 0;
-    input.forEach(number => {
-      if (number[n] === '1') {
+		let ones = 0;
+		input.forEach(number => {
+			if (number[n] === '1') {
 
-        ones++;
-      }
-    })
+				ones++;
+			}
+		});
 
-    gamma = ones > input.length / 2 ? gamma.concat('1') : gamma.concat('0');
-    epsilon = ones > input.length / 2 ? epsilon.concat('0') : epsilon.concat('1');
-  }
+		gamma = ones > input.length / 2 ? gamma.concat('1') : gamma.concat('0');
+		epsilon = ones > input.length / 2 ? epsilon.concat('0') : epsilon.concat('1');
+	}
 
-  return parseInt(gamma,2) * parseInt(epsilon,2)
+	return parseInt(gamma,2) * parseInt(epsilon,2);
 }
 
 function getGasRating(values, doGetOxygen, depth = 0) {
 
-  if (values.length === 1) {
+	if (values.length === 1) {
 
-    return parseInt(values[0],2);
-  } else {
+		return parseInt(values[0],2);
+	} else {
 
-    let filtered = [];
-    let ones = 0;
-    values.forEach(number => {
-      if (number[depth] === '1') {
+		let filtered = [];
+		let ones = 0;
+		values.forEach(number => {
+			if (number[depth] === '1') {
 
-        ones++;
-      }
-    })
-    if (doGetOxygen) {
+				ones++;
+			}
+		});
+		if (doGetOxygen) {
 
-      filtered = ones >= values.length / 2 ? values.filter(el => el[depth] === '1') : values.filter(el => el[depth] === '0')
-    } else {
+			filtered = ones >= values.length / 2 ? values.filter(el => el[depth] === '1') : values.filter(el => el[depth] === '0');
+		} else {
 
-      filtered = ones < values.length / 2 ? values.filter(el => el[depth] === '1') : values.filter(el => el[depth] === '0')
-    }
+			filtered = ones < values.length / 2 ? values.filter(el => el[depth] === '1') : values.filter(el => el[depth] === '0');
+		}
 
-    return getGasRating(filtered, doGetOxygen, ++depth)
-  }
+		return getGasRating(filtered, doGetOxygen, ++depth);
+	}
 }
 
 console.log('result1: ', getPowerConsumption());
-console.log('result2: ', getGasRating(input, true) * getGasRating(input, false))
+console.log('result2: ', getGasRating(input, true) * getGasRating(input, false));
