3c8e84c1f4b2be75dc09f1c795458a1002835bc6
Manuel werder <manuel@docomondo.com>
2021-12-09 15:29:59 +0100

    🎄 day8

---
+++ day8/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', 'utf-8')
+	.trim()
+	.split('\n');
+// DAY 7
+
+console.log('input:', input);
+
+function getUniqueDigits(lines) {
+
+	const outputValues = lines.map(e => e.split(' | ')[1]);
+	console.log('outputValues:', outputValues);
+	const outputValueArray = [];
+	outputValues.forEach(el => {
+
+		const lineArray = el.split(' ');
+		lineArray.forEach(e => {
+			outputValueArray.push(e);
+		});
+	});
+
+	return outputValueArray.filter(el => el.length === 2 | el.length === 3 | el.length === 4 | el.length === 7).length;
+}
+
+// part 1
+console.log('PART 1');
+const result = getUniqueDigits(input);
+console.log('result:', result);
