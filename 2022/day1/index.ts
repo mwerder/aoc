acebe6d86a0a12dad2f1839a96eb9f07ea6f6823
mwerder <manuel.werder@gmail.com>
2022-12-01 14:03:12 +0100

    🎄 day1 improvement
    🎄 day1 improvement

--- 2022/day1/index.ts
+++ 2022/day1/index.ts
@@ -9,21 +9,20 @@ let elveCalories: number[] = [0];
 input.forEach((el: string) => {
 	
 	if (el !== '') {
-		elveCalories[elveCalories.length-1] !== undefined ? elveCalories[elveCalories.length-1] += parseInt(el) : elveCalories[elveCalories.length-1] = parseInt(el);
+		elveCalories[elveCalories.length-1] += parseInt(el);
 	} else {
 		elveCalories.push(0);
 	}
 });
 
 // part 1
-// find index of max value in elveCalories
+// find the max value in elveCalories
 const maxCalories = Math.max(...elveCalories);
 console.log(`Result part 1`, maxCalories);
 
-
 // part 2
 // get the sum of the top three carriers
-const caloriesTopThreeElves = elveCalories.sort((a,b)=>a-b).reverse().slice(0,3).reduce((a,b)=>a+b);
+const caloriesTopThreeElves = elveCalories.sort((a,b)=>b-a).slice(0,3).reduce((a,b)=>a+b);
 
 console.log(`Result part 2`, caloriesTopThreeElves);
 
